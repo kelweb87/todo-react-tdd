c79ea09fc4d2fd04898f56ee4688d4353707befa
Steven Kelly <kelweb87@gmail.com>
2018-03-19 21:57:33 +0000

    another e2e test

---
+++ src/components/addTodo/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AddTodo = () => (
+  <div>
+    <form>
+      <input className="todo-input" />
+      <button type="submit" className="todo-submit">
+        Add a todo
+      </button>
+    </form>
+  </div>
+
+);
+
+export default AddTodo;
---
+++ src/components/addTodo/test.js
@@ -0,0 +1,22 @@
+/* global expect, it, describe */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import AddTodo from '.';
+
+describe('AddTodo component', () => {
+  it('Should render successfully', () => {
+    const component = shallow(<AddTodo />);
+    expect(component.exists()).toEqual(true);
+  });
+  it('Should have one input', () => {
+    const component = shallow(<AddTodo />);
+    expect(component.find('.todo-input').length).toEqual(1);
+  });
+  describe('Add todo button', () => {
+    it('Should exist', () => {
+      const component = shallow(<AddTodo />);
+      expect(component.find('.todo-submit').length).toEqual(1);
+    });
+  });
+});
